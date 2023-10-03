0356742bcd2e5ab3d73d73e32c06f24cae5b82db
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-10-03 20:34:50 +0300

    Define the UserRoles association table

--- server/models/role.py
+++ server/models/role.py
@@ -4,3 +4,11 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    instructor_id =db.Column(db.Integer(), db.ForeignKey('student.id', ondelete='CASCADE'))
+    student_id = db.Column(db.Integer(), db.ForeignKey('student.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
