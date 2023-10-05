8eb55608c40bf9204a2611901557fc8516b2cbc1
arshavinewaema <arshavinewaema@gmail.com>
2023-10-05 20:25:01 +0300

    added InstructorPage with tables and stats

--- client/src/components/InstructorPage.js
+++ client/src/components/InstructorPage.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import React from "react";
+import Hero from "../components/Hero";
+import TopHeader from "../components/TopHeader";
+import InstructorStats from "../components/InstructorStats";
+import ListTable from "../components/ListTable";
 
 function Instructor() {
   const [{ data: instructor, error, status }, setInstructor] = useState({
@@ -8,6 +13,35 @@ function Instructor() {
     status: "pending",
   });
   const { id } = useParams();
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "Student No",
+      width: 160,
+    },
+    { field: "student_name", 
+      headerName: "Student Name", 
+      width: 180 
+    },
+    ,
+    {
+      field: "email_address",
+      headerName: "Email Address",
+      width: 250,
+    },
+    {
+      field: "grade",
+      headerName: "Current Grade",
+      width: 160,
+    },
+    {
+      field: "attendance",
+      headerName: "Average Attedance",
+      width: 170,
+    }
+  ];
+
 
   useEffect(() => {
     fetch(`/instructors/${id}`).then((r) => {
@@ -26,26 +60,36 @@ function Instructor() {
   if (status === "pending") return <h1>Loading...</h1>;
   if (status === "rejected") return <h1>Error: {error.error}</h1>;
 
-  return (
-    <section>
-      <h3>Name: {instructor.name}</h3>
-      <h3>Staff Number: {instructor.staff_number}</h3>
-      <h3>Email : {instructor.email_address}%</h3>
-    
-      <h3>students:</h3>
-      <ul>
-        {instructor.students.map((student) => (
-          <li key={instructor.id}>
-            <Link to={`/students/${student.id}`}>{student.name}</Link>
-            {/* <Link to={`/students/${student.id}`}>{student.student_number}</Link>
-            <Link to={`/students/${student.id}`}>{student.email_address}</Link>                
-            <Link to={`/students/${student.id}`}>{student.grade}</Link>
-            <Link to={`/students/${student.id}`}>{student.attendance}</Link> */}
-            </li>
-        ))}
-      </ul>
 
-    </section>
+  const title = `Hello, ${instructor.name}`;
+  const description =
+    "Dolor sit amet consectetur adipiscing elit ut aliquam purus sit. Urna condimentum mattis pellentesque id nibh tortor id. Ultrices gravida dictum fusce ut placerat orci nulla pellentesque dignissim.";
+  const image = "/assets/images/image-5.png";
+  const student_number = instructor.students.length
+
+  return (
+
+    <div>
+      <TopHeader />
+      <Hero title={title} description={description} image={image} />
+      <InstructorStats
+        instructorName={instructor.name}
+        instructorNumber={instructor.staff_number}
+        units={5}
+        studentsCoaching={student_number}
+      />
+      <ListTable
+        headers={columns}
+        data={instructor.students.map((student) => ({
+          id: student.student_number,
+          student_name: student.name,
+          email_address: student.email_address,
+          grade: `${student.grade}%`,
+          attendance: `${student.attendance}%`,
+        }))}
+        title='List of Students'
+      />
+    </div>
   );
 }
 
