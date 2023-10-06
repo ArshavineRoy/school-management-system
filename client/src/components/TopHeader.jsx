489b182fa86916054d5c92132de5c7b6e5c5b473
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-10-06 09:34:38 +0300

    Implemented logout functionality in TopHeader component

--- client/src/components/TopHeader.jsx
+++ client/src/components/TopHeader.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const TopHeader = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    // Clear the authentication token from local storage (assuming it's stored as "token")
+    localStorage.removeItem("token");
+
+    // Redirect the user to the login page (you should replace "/login" with your actual login route)
+    history.push("/");
+  };
+
   return (
-    <div className='h-10 z-20 fixed w-full bg-color-main flex justify-end text-white align-center px-8'>
+    <div className='h-10 z-20 fixed w-full bg-color-main flex justify-between items-center px-8 text-white'>
       <h1 className='text-2xl'>
         <span className='uppercase text-light-green'>Bamac</span> Student Portal
       </h1>
+      <button
+        onClick={handleLogout}
+        className='text-sm text-white bg-red-500 px-3 py-1 rounded hover:bg-red-600 cursor-pointer'
+      >
+        Logout
+      </button>
     </div>
   );
 };
